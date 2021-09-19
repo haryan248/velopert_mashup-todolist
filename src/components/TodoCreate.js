464fb14dfbe3e3c481729a5902e7b25fb6f00c38
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2021-09-19 22:17:36 +0900

    [add] 할일 추가하기 기능

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { MdAdd } from "react-icons/md";
+import { useTodoDispatch, useTodoNextId } from "../TodoContext";
 
 const CircleButton = styled.button`
     background: #38d9a9;
@@ -78,17 +79,36 @@ const Input = styled.input`
 
 function TodoCreate() {
     // +,x 버튼 상태 관리
+
     const [open, setOpen] = useState(false);
+    const [value, setValue] = useState("");
+
+    const dispatch = useTodoDispatch();
+    const nextId = useTodoNextId();
     // open button 토글
     const onToggle = () => setOpen(!open);
-
+    const onChange = (e) => setValue(e.target.value);
+    const onSubmit = (e) => {
+        e.preventDefault(); // 새로고침 방지
+        dispatch({
+            type: "CREATE",
+            todo: {
+                id: nextId.current,
+                text: value,
+                done: false,
+            },
+        });
+        setValue("");
+        setOpen(false);
+        nextId.current += 1;
+    };
     return (
         <>
             {/* open 시 입력 창 보이도록 하기 */}
             {open && (
                 <InsertFormPositioner>
-                    <InsertForm>
-                        <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" />
+                    <InsertForm onSubmit={onSubmit}>
+                        <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" onChange={onChange} value={value} />
                     </InsertForm>
                 </InsertFormPositioner>
             )}
@@ -98,5 +118,5 @@ function TodoCreate() {
         </>
     );
 }
-
-export default TodoCreate;
+// TodoContext 에서 관리하고 있는 state 가 바뀔 때 때 TodoCreate 의 불필요한 리렌더링을 방지
+export default React.memo(TodoCreate);
